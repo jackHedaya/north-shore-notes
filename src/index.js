09c41be2f0d037c49bf66d82e879617b3ff1bdde
Jack Hedaya <jackehedaya@gmail.com>
2019-05-06 13:36:54 -0400

    changed routers and reformated

--- src/index.js
+++ src/index.js
@@ -5,20 +5,21 @@ const mongoose = require("mongoose");
 const authMiddleware = require("./routers/middleware");
 
 const routers = {
-  accounts: require("./routers/accounts")
+  accounts: require("./routers/accounts"),
+  main: require("./routers/main")
 };
 
 const PORT = 3000 || process.env.PORT;
 
 // Edit according to your project
-mongoose.connect("mongodb://localhost/project_name");
+mongoose.connect("mongodb://localhost/north-shore-notes");
 
 app.use(bodyParser.urlencoded({ extended: false, limit: "2mb" }));
 app.use(bodyParser.json({ limit: "2mb" }));
 
-app.use(routers.accounts);
+app.use("/auth", routers.accounts);
 
 // Whatever you want that requires auth
-app.use("/needsAuth", authMiddleware);
+app.use("/", authMiddleware, routers.main);
 
 app.listen(PORT, () => console.log(`Backend listening on port ${PORT}`));
