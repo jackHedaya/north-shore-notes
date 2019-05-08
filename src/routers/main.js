d617f2a6337d9d8ef490c3721156b2b4f726b4c0
Jack Hedaya <jackehedaya@gmail.com>
2019-05-08 13:37:22 -0400

    added endpoints for adding an issue and image

--- src/routers/main.js
+++ src/routers/main.js
@@ -1,16 +1,25 @@
 const app = require("express").Router();
 
-const Blog = require("../db/schemas/blog");
+const Issue = require("../db/schemas/issue");
+const ArtImage = require("../db/schemas/art_image");
 
-app.post("/blog", async (req, res) => {
-  const { blogTitle, blogBody } = req.body;
+app.post("/issue", async (req, res) => {
+  const { issue_title, issue_body } = req.body;
   const currentTime = Date.now();
 
-  const { account_id } = req.body.account;
+  const { account_id } = req.account;
 
-  Blog.create({ account_id: account_id, title: blogTitle, body: blogBody, date: currentTime })
-    .then(x => res.status(200).send())
-    .catch(x => res.status(400).send({ error: "Missing fields" }));
+  Issue.create({ account_id: account_id, title: issue_title, body: issue_body, date: currentTime })
+    .then(_ => res.status(200).send())
+    .catch(_ => res.status(400).send({ error: "Missing fields" }));
+});
+
+app.post("/art_image", async (req, res) => {
+  const { base64, image_author } = req.body;
+
+  ArtImage.create({ base64, author: image_author })
+  .then(_ => res.status(200).send())
+  .catch(_ => res.status(400).send({ message: "Something went wrong saving the image" }))
 });
 
 module.exports = app;
