8f9aa11c72ae279d991789ce8f977892ad581934
Ron Laniado <rlaniado24@gmail.com>
2019-05-12 15:30:07 -0400

    Added the basic page navigation using react-router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { HashRouter, Route, Link } from "react-router-dom";
 
 function App() {
   const [issues, setIssues] = useState([]);
@@ -12,6 +13,15 @@ function App() {
 
   return (
     <div className="App">
+      <HashRouter>
+        <ul>
+          <li><Link to="/this-week/">THIS WEEK</Link></li>
+          <li><Link to="/last-week">LAST WEEK</Link></li>
+          <li><Link to="/previous-issues/">PREVIOUS ISSUES</Link></li>
+          <li><Link to="/student-art">STUDENT ART</Link></li>
+          <li><Link to="/about/">ABOUT</Link></li>
+        </ul>
+      </HashRouter>
       {issues.map(issue => (
         <div>
           <div>{issue.title}</div>
