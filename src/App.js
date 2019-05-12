6e3452393114455f3c8a834d540e84f802616ba4
Jack Hedaya <jackehedaya@gmail.com>
2019-05-12 14:26:27 -0400

    fetches issues from localhost and dispays them

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [issues, setIssues] = useState([]);
+
+  useEffect(() => {
+    fetch("/issue", {})
+      .then(resp => resp.json())
+      .then(body => setIssues(body))
+      .catch(e => console.log(e));
+  }, []);
+
   return (
     <div className="App">
-      <div>Hello, World!</div>
+      {issues.map(issue => (
+        <div>
+          <div>{issue.title}</div>
+          <div>{issue.body}</div>
+        </div>
+      ))}
     </div>
   );
 }
