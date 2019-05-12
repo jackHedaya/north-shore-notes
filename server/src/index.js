22b3f3c99047525c2184e169d949bf528c7b3423
Jack Hedaya <jackehedaya@gmail.com>
2019-05-12 12:19:03 -0400

    broke routers into public and auth_required, serve client

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,18 @@
-const app = require("express")();
+const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 
+const app = express();
 const authMiddleware = require("./routers/middleware");
 
 const routers = {
   accounts: require("./routers/accounts"),
-  main: require("./routers/main")
+  auth_required: require("./routers/auth_required"),
+  public: require("./routers/public")
 };
 
-const PORT = 3000 || process.env.PORT;
+const PORT = 3001 || process.env.PORT;
 
 // Edit according to your project
 mongoose.connect("mongodb://localhost/north-shore-notes");
@@ -17,9 +20,16 @@ mongoose.connect("mongodb://localhost/north-shore-notes");
 app.use(bodyParser.urlencoded({ extended: false, limit: "2mb" }));
 app.use(bodyParser.json({ limit: "2mb" }));
 
+app.use(express.static(path.join(__dirname, "build")));
+
 app.use("/auth", routers.accounts);
 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 // Whatever you want that requires auth
-app.use("/", authMiddleware, routers.main);
+app.use("/", routers.public);
+app.use("/", authMiddleware, routers.auth_required);
 
 app.listen(PORT, () => console.log(`Backend listening on port ${PORT}`));
