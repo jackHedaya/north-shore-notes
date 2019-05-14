c9a720ccaba43b5b1451ea503fd32e6d5126c8fe
Jack Hedaya <jackehedaya@gmail.com>
2019-05-14 15:47:32 -0400

    header complete

--- src/pages/this-week/ThisWeek.js
+++ src/pages/this-week/ThisWeek.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Collapse from '@material-ui/core/Collapse';
+import Collapse from "@material-ui/core/Collapse";
 
 import angleDown from "../../assets/angle-down.svg";
 
@@ -12,9 +12,24 @@ function ThisWeek() {
 
   return (
     <div className="this-week">
+      <Header />
       {issues.map(({ title, body, author = "Anonymous" }) => (
         <Issue title={title} body={body} author={author} key={title} />
       ))}
+    </div>
+  );
+}
+
+function Header() {
+  const { weekday, month, date, year } = getDateInformation();
+
+  return (
+    <div>
+      <div className="main-header">
+        <div className="volume">Volume X Issue Y</div>
+        <div className="date">{`${weekday}, ${month} ${date}, ${year}`}</div>
+      </div>
+      <div className="break" />
     </div>
   );
 }
@@ -49,10 +64,38 @@ function ExpandoBody(props) {
   const { show } = props;
 
   return (
-    <Collapse in={show} classes={{ wrapperInner: 'body'}}>
+    <Collapse in={show} classes={{ wrapperInner: "body" }}>
       {props.children}
     </Collapse>
   );
+}
+
+/** Helper Functions */
+function getDateInformation() {
+  const now = new Date();
+
+  const NUM_TO_DAY = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const NUM_TO_MONTH = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  return {
+    weekday: NUM_TO_DAY[now.getDay()],
+    month: NUM_TO_MONTH[now.getMonth()],
+    date: now.getUTCDate(),
+    year: now.getUTCFullYear()
+  };
 }
 
 export default ThisWeek;
