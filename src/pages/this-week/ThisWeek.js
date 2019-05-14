f74b7432cb643695ccd33071c7ad10ae6d7e0813
Jack Hedaya <jackehedaya@gmail.com>
2019-05-14 11:48:23 -0400

    body is now expandable

--- src/pages/this-week/ThisWeek.js
+++ src/pages/this-week/ThisWeek.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import angleDown from "../../assets/angle-down.svg";
 
@@ -12,16 +12,39 @@ function ThisWeek() {
   return (
     <div className="this-week">
       {issues.map(({ title, body, author = "Anonymous" }) => (
-        <div className="issue" key={title}>
-          <div className="info">
-            <div className="title">{title} </div>
-            <div className="author">By: {author}</div>
-          </div>
-          <div className="dropdown-icon">
-            <img className="angle-down" src={angleDown} alt="angle-down" />
-          </div>
-        </div>
+        <Issue title={title} body={body} author={author} key={title} />
       ))}
+    </div>
+  );
+}
+
+function Issue(props) {
+  const { title, body, author } = props;
+
+  const [showBody, setShowBody] = useState(false);
+
+  return (
+    <div className="issue">
+      <div className="header">
+        <div className="info">
+          <div className="title">{title} </div>
+          <div className="author">By: {author}</div>
+        </div>
+        <div className="dropdown-icon" onClick={() => setShowBody(!showBody)}>
+          <img className="angle-down" src={angleDown} alt="angle-down" />
+        </div>
+      </div>
+      <ExpandoBody show={showBody}>{body}</ExpandoBody>
+    </div>
+  );
+}
+
+function ExpandoBody(props) {
+  const { show } = props;
+
+  return (
+    <div className="body" style={{ display: show ? undefined : "none" }}>
+      {props.children}
     </div>
   );
 }
