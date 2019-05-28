d3c806ba81cf37892bacdd6ac96abd6b2ebe9107
Ron <laniado.ron@gmail.com>
2019-05-28 00:45:30 -0400

    Moved component around for organization

--- src/App.js
+++ src/App.js
@@ -10,23 +10,24 @@ import LastWeek from "./pages/last-week/LastWeek";
 import StudentArt from "./pages/student-art/StudentArt";
 import PreviousIssues from "./pages/previous-issues/PreviousIssues";
 
-import "./App.scss"
+import "./App.scss";
 
 function App() {
-  return (
-    <Router>
-      <Navigation />
-
-      <div className="content">
-        <Route exact path="/" component={ThisWeek} />
-        <Route exact path="/this-week/" component={ThisWeek} />
-        <Route exact path="/last-week/" component={LastWeek} />
-        <Route exact path="/previous-issues/" component={PreviousIssues} />
-        <Route exact path="/student-art/" component={StudentArt} />
-        <Route exact path="/about/" component={About} />
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="container">
+				<Navigation />
+				<div className="content">
+					<Route exact path="/" component={ThisWeek} />
+					<Route exact path="/this-week/" component={ThisWeek} />
+					<Route exact path="/last-week/" component={LastWeek} />
+					<Route exact path="/previous-issues/" component={PreviousIssues} />
+					<Route exact path="/student-art/" component={StudentArt} />
+					<Route exact path="/about/" component={About} />
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
