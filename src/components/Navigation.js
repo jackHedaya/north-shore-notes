633bd7dd13052f95f72ee81aeb10aa3f96779f82
Ron <laniado.ron@gmail.com>
2019-05-28 00:46:32 -0400

    Switched from vert navbar to horizontal navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,44 +1,41 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
-
 import "./_styles/Navigation.scss";
 
-export default function Navigation() {
-  return (
-    <>
-      <ul className="navigation">
-        <Link to="/" className="navItem">
-          <Logo />
-        </Link>
-        <NavLink to="/this-week/">THIS WEEK</NavLink>
-        <NavLink to="/last-week/">LAST WEEK</NavLink>
-        <NavLink to="/previous-issues/">PREVIOUS ISSUES</NavLink>
-        <NavLink to="/student-art/">STUDENT ART</NavLink>
-        <NavLink to="/about/">ABOUT</NavLink>
-      </ul>
-    </>
-  );
+export default class Navigation extends React.Component {
+	addShadow() {
+		console.log("scrolled!!!");
+	}
+	render() {
+		return (
+			<ul className={"navigation"}>
+				<Link to="/" className="navItem">
+					NSN
+				</Link>
+				<div className="tabs-container">
+					<div className="other-tabs">
+						<NavLink to="/about/">ABOUT</NavLink>
+						<NavLink to="/student-art/">STUDENT ART</NavLink>
+					</div>
+					<div className="article-tabs">
+						<NavLink to="/previous-issues/">PREVIOUS ISSUES</NavLink>
+						<NavLink to="/last-week/">LAST WEEK</NavLink>
+						<NavLink to="/this-week/">THIS WEEK</NavLink>
+					</div>
+				</div>
+			</ul>
+		);
+	}
 }
 
-const NavLink = withRouter(props => {
-  const isSelected = () => props.location.pathname.includes(props.to);
+const NavLink = withRouter((props) => {
+	const isSelected = () => props.location.pathname.includes(props.to);
 
-  return (
-    <li>
-      <Link to={props.to} className={isSelected() ? "selected" : null}>
-        {props.children}
-      </Link>
-    </li>
-  );
+	return (
+		<li>
+			<Link to={props.to} className={isSelected() ? "selected" : null}>
+				{props.children}
+			</Link>
+		</li>
+	);
 });
-
-function Logo() {
-  return (
-    <img
-      src="https://liveparker301.com/wp-content/uploads/2015/10/placeholder-circle.png"
-      alt="logo"
-      height="100"
-      width="100"
-    />
-  );
-}
