e1662132e18a5d6d8b341ac045ab0fc64f981f5d
Jack Hedaya <jackehedaya@gmail.com>
2019-06-02 00:12:30 -0400

    redid ThisWeek for new design

--- src/pages/this-week/ThisWeek.js
+++ src/pages/this-week/ThisWeek.js
@@ -1,7 +1,4 @@
-import React, { useState, Fragment } from "react";
-import Collapse from "@material-ui/core/Collapse";
-
-import angleDown from "../../assets/angle-down.svg";
+import React from "react";
 
 import useIssues from "../../hooks/useIssues";
 
@@ -12,24 +9,9 @@ function ThisWeek() {
 
   return (
     <div className="this-week">
-      <Header />
-      {issues.map(({ title, body, author = "Anonymous" }) => (
-        <Issue title={title} body={body} author={author} key={title} />
+      {issues.map(({ title, body, author = "Anonymous" }, index) => (
+        <Issue title={title} body={body} author={author} key={title} break={index !== issues.length - 1} />
       ))}
-    </div>
-  );
-}
-
-function Header() {
-  const { weekday, month, date, year } = getDateInformation();
-
-  return (
-    <div>
-      <div className="main-header">
-        <div className="volume">Volume X Issue Y</div>
-        <div className="date">{`${weekday}, ${month} ${date}, ${year}`}</div>
-      </div>
-      <div className="break" />
     </div>
   );
 }
@@ -37,60 +19,14 @@ function Header() {
 function Issue(props) {
   const { title, body, author } = props;
 
-  const [showBody, setShowBody] = useState(false);
-
   return (
     <div className="issue">
-      <div className="header" onClick={() => setShowBody(!showBody)}>
-        <div className="info">
-          <div className="title">{title} </div>
-          <div className="author">By: {author}</div>
-        </div>
-        <div className="dropdown-icon">
-          <img className={"angle-down " + (showBody ? "flipped" : "")} src={angleDown} alt="angle-down" />
-        </div>
-      </div>
-      <ExpandoBody show={showBody}>{body}</ExpandoBody>
+      <div className="title">{title}</div>
+      <div className="author">By: {author}</div>
+      <div className="body">{body}</div>
+      {props.break ? <div className="break" /> : null}
     </div>
   );
-}
-
-function ExpandoBody(props) {
-  const { show } = props;
-
-  return (
-    <Collapse in={show} classes={{ wrapperInner: "body" }}>
-      {props.children}
-    </Collapse>
-  );
-}
-
-/** Helper Functions */
-function getDateInformation() {
-  const now = new Date();
-
-  const NUM_TO_DAY = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  const NUM_TO_MONTH = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ];
-
-  return {
-    weekday: NUM_TO_DAY[now.getDay()],
-    month: NUM_TO_MONTH[now.getMonth()],
-    date: now.getUTCDate(),
-    year: now.getUTCFullYear()
-  };
 }
 
 export default ThisWeek;
