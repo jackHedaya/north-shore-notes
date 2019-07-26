f9fa69932523494d88cf46b235ce0b093138cf3d
Jack Hedaya <jackehedaya@gmail.com>
2019-07-26 17:26:56 -0600

    PreviousIssues now displays all issues in order

--- src/pages/previous-issues/PreviousIssues.js
+++ src/pages/previous-issues/PreviousIssues.js
@@ -1,8 +1,34 @@
 import React from "react";
 import "./PreviousIssues.scss";
+import useIssue from "../../hooks/useIssue";
+import IssueDisplay from "../../components/IssueDisplay";
 
-export default class PreviousIssues extends React.Component {
-	render() {
-		return <div>This is the previous issues page</div>;
-	}
+function PreviousIssues() {
+    const issues = useIssue({ volume: "ALL", issue: "ALL" });
+		const groupedIssues = groupByIssue(issues);		
+
+		return <div>{groupedIssues.map(issue => <IssueDisplay key={`${issue[0].issue} ${issue[0].volume}`} issue={issue} /> )}</div>;
+  
 }
+
+function groupByIssue(articles) {
+	if (articles.length < 1)
+		return [];
+
+	let out = [[ articles[0] ]];
+
+	for (let i = 1; i < articles.length; i++) {
+		const current = articles[i];
+		const last = articles[i - 1];
+
+		if (current.volume !== last.volume && current.issue !== last.issue) {
+			out.push([]);
+		}
+
+		out[ out.length - 1].push(current)
+	}
+
+	return out;
+}
+
+export default PreviousIssues;
