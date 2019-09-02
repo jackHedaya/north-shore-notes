6610824a332b17008711aebe5180162271623269
Jack Hedaya <jackehedaya@gmail.com>
2019-09-01 23:00:41 -0400

    added Login page and changed styling

---
+++ src/pages/login/Login.js
@@ -0,0 +1,77 @@
+import React, { useState, useContext } from "react";
+import { Redirect } from "react-router-dom";
+import Loader from "react-loader-spinner";
+
+import AuthContext from "../../Context";
+
+import * as authenticationService from "../../services/authentication.service";
+
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import "./Login.scss";
+
+function Login(props) {
+  const { setToken, isLoggedIn, setLoggedIn } = useContext(AuthContext);
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isInvalid, setIsInvalid] = useState(false);
+
+  function login() {
+    setIsLoading(true);
+    authenticationService
+      .login(username, password)
+      .then(resp => {
+        setToken(resp.data.jwt);
+        setLoggedIn(true);
+      })
+      .catch(_ => {
+        setIsInvalid(true);
+        setTimeout(() => setIsInvalid(false), 3000);
+      })
+      .finally(_ => setIsLoading(false));
+  }
+
+  return isLoggedIn ? (
+    <Redirect to={props.location.state ? props.location.state.from.pathname : "/"} />
+  ) : isLoading ? (
+    <Loader type="CradleLoader" color="#78E88E" width="100" height="100" />
+  ) : (
+    <div className="login">
+      <div className="volume-issue-title">Login</div>
+      <div className="inputs">
+        <Input type="text" name="username" value={username} onChange={e => setUsername(e.currentTarget.value)} />
+        <Input type="password" name="password" value={password} onChange={e => setPassword(e.currentTarget.value)} />
+        <LoginButton loading={isLoading} login={login} error={isInvalid} />
+      </div>
+    </div>
+  );
+}
+
+function Input(props) {
+  return (
+    <>
+      <div className="name">{props.name}</div>
+      <input {...props} />
+    </>
+  );
+}
+
+function LoginButton(props) {
+  return (
+    <div
+      className="submit"
+      style={{
+        width: props.loading ? "30px" : undefined,
+        backgroundColor: props.error ? "red" : undefined,
+        textIndent: props.loading ? 30 : undefined
+      }}
+      onClick={props.login}
+    >
+      Login
+    </div>
+  );
+}
+
+export default Login;
