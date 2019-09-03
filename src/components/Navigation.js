3b20fa9d8457e6511c79cbd7515381df92a5177b
Jack Hedaya <jackehedaya@gmail.com>
2019-09-02 23:57:34 -0400

    added greeting and user dropdown

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,38 +1,63 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { Link, withRouter } from "react-router-dom";
+import { Dropdown, DropdownHeader, DropdownMenu, DropdownToggle, DropdownItem } from "reactstrap";
+
+import { AuthContext } from "../App";
+import useUser from "../hooks/useUser";
+
 import "./_styles/Navigation.scss";
 
 function Navigation() {
-		return (
-			<ul className="navigation">
-				<Link to="/" className="navItem">
-					NSN
-				</Link>
-				<div className="tabs-container">
-					<div className="other-tabs">
-						<NavLink to="/about/">ABOUT</NavLink>
-						<NavLink to="/student-art/">STUDENT ART</NavLink>
-					</div>
-					<div className="article-tabs">
-						<NavLink to="/previous-issues/">PREVIOUS ISSUES</NavLink>
-						<NavLink to="/last-week/">LAST WEEK</NavLink>
-						<NavLink to="/this-week/">THIS WEEK</NavLink>
-					</div>
-				</div>
-			</ul>
-		);
+  const { isLoggedIn, token } = useContext(AuthContext);
+  const user = useUser(token);
+
+  return (
+    <ul className="navigation">
+      <Link to="/" className="nsn">
+        NSN
+      </Link>
+      <div className="tabs-container">
+        <div className="other-tabs">
+          <NavLink to="/about/">ABOUT</NavLink>
+          <NavLink to="/student-art/">STUDENT ART</NavLink>
+        </div>
+        <div className="article-tabs">
+          <NavLink to="/previous-issues/">PREVIOUS ISSUES</NavLink>
+          <NavLink to="/last-week/">LAST WEEK</NavLink>
+          <NavLink to="/this-week/">THIS WEEK</NavLink>
+          <NavLink to="/this-week/">THIS WEEK</NavLink>
+          {isLoggedIn && user && <CustomDropdown name={user.first_name} />}
+        </div>
+      </div>
+    </ul>
+  );
 }
 
-const NavLink = withRouter((props) => {
-	const isSelected = () => props.location.pathname.includes(props.to);
+const NavLink = withRouter(props => {
+  const isSelected = () => props.location.pathname.includes(props.to);
 
-	return (
-		<li>
-			<Link to={props.to} className={isSelected() ? "selected" : null}>
-				{props.children}
-			</Link>
-		</li>
-	);
+  return (
+    <li>
+      <Link to={props.to} className={isSelected() ? "selected" : null}>
+        {props.children}
+      </Link>
+    </li>
+  );
 });
+
+function CustomDropdown(props) {
+  const [open, setOpen] = useState(false);
+  const toggle = () => setOpen(!open);
+
+  return (
+    <Dropdown isOpen={open} toggle={toggle} nav>
+      <DropdownToggle caret className="greeting">HELLO, {props.name.toUpperCase()}</DropdownToggle>
+      <DropdownMenu right>
+        <DropdownItem>Manage Users</DropdownItem>
+        <DropdownItem>Log Out</DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+}
 
 export default Navigation;
