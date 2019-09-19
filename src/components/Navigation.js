240443a8971c6638f32029594352d061ec52ac5f
Ron <laniado.ron@gmail.com>
2019-09-19 11:46:32 -0400

    Changed code styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,36 +3,36 @@ import { Link, withRouter } from "react-router-dom";
 import "./_styles/Navigation.scss";
 
 function Navigation() {
-		return (
-			<ul className="navigation">
-				<Link to="/" className="navItem">
-					NSN
-				</Link>
-				<div className="tabs-container">
-					<div className="other-tabs">
-						<NavLink to="/about/">ABOUT</NavLink>
-						<NavLink to="/student-art/">STUDENT ART</NavLink>
-					</div>
-					<div className="article-tabs">
-						<NavLink to="/previous-issues/">PREVIOUS ISSUES</NavLink>
-						<NavLink to="/last-week/">LAST WEEK</NavLink>
-						<NavLink to="/this-week/">THIS WEEK</NavLink>
-					</div>
-				</div>
-			</ul>
-		);
+  return (
+    <ul className="navigation">
+      <Link to="/" className="navItem">
+        NSN
+      </Link>
+      <div className="tabs-container">
+        <div className="other-tabs">
+          <NavLink to="/about/">ABOUT</NavLink>
+          <NavLink to="/student-art/">STUDENT ART</NavLink>
+        </div>
+        <div className="article-tabs">
+          <NavLink to="/previous-issues/">PREVIOUS ISSUES</NavLink>
+          <NavLink to="/last-week/">LAST WEEK</NavLink>
+          <NavLink to="/this-week/">THIS WEEK</NavLink>
+        </div>
+      </div>
+    </ul>
+  );
 }
 
-const NavLink = withRouter((props) => {
-	const isSelected = () => props.location.pathname.includes(props.to);
+const NavLink = withRouter(props => {
+  const isSelected = () => props.location.pathname.includes(props.to);
 
-	return (
-		<li>
-			<Link to={props.to} className={isSelected() ? "selected" : null}>
-				{props.children}
-			</Link>
-		</li>
-	);
+  return (
+    <li>
+      <Link to={props.to} className={isSelected() ? "selected" : null}>
+        {props.children}
+      </Link>
+    </li>
+  );
 });
 
 export default Navigation;
