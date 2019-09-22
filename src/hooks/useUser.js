906795e8093d5f3f4d559460b83468f17e0d13a9
Jack Hedaya <jackehedaya@gmail.com>
2019-09-22 17:15:40 -0400

    manage users WIP

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,16 +1,16 @@
 import { useState, useEffect } from "react";
 
-export default token => {
+export default (token, qUser) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
     if (!token) return undefined;
 
-    fetch(`/user`, { headers: { Authorization: `Bearer ${token}` } })
+    fetch(`/user?${qUser ? "user=" + qUser : ""}`, { headers: { Authorization: `Bearer ${token}` } })
       .then(resp => resp.json())
       .then(body => setUser(body))
       .catch(_ => setUser(null));
-  }, [token]);
+  }, [token, qUser]);
 
   return user;
 };
