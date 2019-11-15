340667d8df4e4b1e4149ce34c5938c7b6e906411
Jack Hedaya <jackehedaya@gmail.com>
2019-11-15 01:00:33 -0500

    editable rows

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -13,8 +13,16 @@ export default function newFetch(url, { method = 'GET', headers, body, ...option
     ...options,
     method,
     body: method !== 'GET' ? JSON.stringify(body) : undefined,
-  }).then(resp => {
-    if (resp.ok) return resp.json()
+  }).then(async resp => {
+    if (resp.ok) {
+      try {
+        const json = await resp.json()
+
+        return json
+      } catch (_) {
+        return Promise.resolve()
+      }
+    }
     throw resp
   })
 }
